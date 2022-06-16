93ef84ae5b968613901466fef77f9da91f7a11da
Sahith03 <sahith.varma.03@gmail.com>
2022-06-16 16:24:57 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name='sahithcalc',
+    version='0.1.1',
+    author="Sahith",
+    author_email="sahith.varma.03@gmail.com",
+    description="Basic calculator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
